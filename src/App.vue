d8d5360b5f56c015bb0d196c6913d61092654fc5
黄明富 <212149997@qq.com>
2025-01-10 10:14:09 +0800

    chore: 优化app.vue代码

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <router-view v-slot="{ Component, route }">
     <transition :name="route.meta.transition || 'fade'">
-      <keep-alive>
-        <component
-          :is="Component"
-          v-if="route.meta.keepAlive"
-          :key="route.name"
-        />
+      <keep-alive v-if="route.meta.keepAlive">
+        <component :is="Component" :key="route.name" />
       </keep-alive>
-    </transition>
-    <transition :name="route.meta.transition || 'fade'">
-      <component
-        :is="Component"
-        v-if="!route.meta.keepAlive"
-        :key="route.name"
-      />
+      <component :is="Component" v-else :key="route.name" />
     </transition>
   </router-view>
 </template>
